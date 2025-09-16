ec7ee2f9002436b4d18f2d757134a0d0b61b7068
Victor Marquez <victormarquez.dev@gmail.com>
2025-09-16 19:16:02 -0400

    REBOOT

--- glob.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'glob' {
-  export interface GlobOptions {
-    cwd?: string;
-  }
-  export function glob(pattern: string, options?: GlobOptions, cb?: (err: Error | null, matches: string[]) => void): void;
-}
